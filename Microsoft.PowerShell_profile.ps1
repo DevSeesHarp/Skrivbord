1705fa211fc1018520ea91070ad320c94dc88db4
Sebastian Lindblom <61666556+DevSeesHarp@users.noreply.github.com>
2023-09-05 14:00:59 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,4 +16,4 @@ Catch {
     Import-Module -Name Terminal-Icons
 }
 oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
-$repos = Resolve-Path -Path "source/repos"
+$repos = Resolve-Path -Path "*/source/repos"
